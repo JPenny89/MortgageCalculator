18140cee9d43c948d42ad924b5b2e4156c101b2c
James Penny <james_penny@hotmail.co.uk>
2023-01-13 16:00:27 +0000

    Added a dropdown menu for: - First Time Buyer - Home Purchase - Remortgage

--- Mortgage Calculator/Controller/MortgageCalculator.swift
+++ Mortgage Calculator/Controller/MortgageCalculator.swift
@@ -8,6 +8,11 @@
 import UIKit
 import Foundation
 
+class CellClass: UITableViewCell {
+    
+    
+}
+
 class MortgageCalculator: UIViewController {
     
 //    var parameters = Parameters(rate: 0.0, loan: 0.0, term: 0.0)
@@ -15,6 +20,7 @@ class MortgageCalculator: UIViewController {
 
     
     
+    @IBOutlet weak var borrowingDropdown: UIButton!
     @IBOutlet weak var interestTextField: UITextField!
     @IBOutlet weak var loanTextField: UITextField!
     @IBOutlet weak var termTextField: UITextField!
@@ -23,10 +29,47 @@ class MortgageCalculator: UIViewController {
     @IBOutlet weak var stampDuty: UILabel!
     @IBOutlet weak var loanToValue: UILabel!
     
+    let transparentView = UIView()
+    let tableView = UITableView()
+    
+    var selectedButton = UIButton()
+    
+    var dataSource = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CellClass.self, forCellReuseIdentifier: "Cell")
     }
+    
+    func addTransparanetView(frames: CGRect) {
+            let window = UIApplication.shared.keyWindow
+            transparentView.frame = window?.frame ?? self.view.frame
+            self.view.addSubview(transparentView)
+        
+        tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+        self.view.addSubview(tableView)
+        tableView.layer.cornerRadius = 5
+            
+            transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+        tableView.reloadData()
+            let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
+            transparentView.addGestureRecognizer(tapgesture)
+            transparentView.alpha = 0
+            UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+                self.transparentView.alpha = 0.5
+                self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: CGFloat(self.dataSource.count * 50))
+            }, completion: nil)
+        }
+        
+        @objc func removeTransparentView() {
+            let frames = selectedButton.frame
+            UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+                self.transparentView.alpha = 0
+                self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height + 5, width: frames.width, height: 0)
+            }, completion: nil)
+        }
     
     func calculateMonthlyRepayment() {
         
@@ -95,14 +138,20 @@ class MortgageCalculator: UIViewController {
         
     }
         
-
+    @IBAction func dropdownClicked(_ sender: Any) {
+        dataSource = ["First Time Buyer", "Home Purchase", "Remortgage"]
+        selectedButton = borrowingDropdown
+        addTransparanetView(frames: borrowingDropdown.frame)
+    }
+    
     @IBAction func calculatePressed(_ sender: Any) {
         
-        calculateMonthlyRepayment()
-        calculateStampDuty()
-        calculateLTV()
-        
-    }
+            calculateMonthlyRepayment()
+            calculateStampDuty()
+            calculateLTV()
+            
+            print("Borrowing Dropdown = \(borrowingDropdown.currentTitle)")
+        }
     
 
 }
@@ -116,5 +165,26 @@ extension MortgageCalculator: UITextFieldDelegate {
         view.endEditing(true)
     }
 
+}
+
+extension MortgageCalculator: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedButton.setTitle(dataSource[indexPath.row], for: .normal)
+        removeTransparentView()
+    }
 }
 
