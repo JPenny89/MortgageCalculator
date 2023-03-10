fef36d7ce21d3db58d411ad9944e07a7074ff599
James Penny <james_penny@hotmail.co.uk>
2023-03-10 11:42:54 +0000

    Added thousands() method to all outputs in the Stamp Duty label.

--- Mortgage Calculator/Controller/MortgageCalculator.swift
+++ Mortgage Calculator/Controller/MortgageCalculator.swift
@@ -9,17 +9,11 @@ import UIKit
 import Foundation
 
 class CellClass: UITableViewCell {
-    
-    
+
 }
 
 class MortgageCalculator: UIViewController {
-    
-//    var parameters = Parameters(rate: 0.0, loan: 0.0, term: 0.0)
-    
 
-    
-    
     @IBOutlet weak var borrowingDropdown: UIButton!
     @IBOutlet weak var interestTextField: UITextField!
     @IBOutlet weak var loanTextField: UITextField!
@@ -36,17 +30,10 @@ class MortgageCalculator: UIViewController {
     
     var dataSource = [String]()
     
-   
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.largeContentTitle = "Title"
-        
-        
-        
-
-        
         monthlyPayment.text = "£0.00"
         stampDuty.text = "£0"
         loanToValue.text = "0%"
@@ -84,6 +71,8 @@ class MortgageCalculator: UIViewController {
             }, completion: nil)
         }
     
+    //MARK: - CALCULATIONS
+    
     func calculateMonthlyRepayment() {
         
         let rate = Double (interestTextField.text!) ?? 0.0
@@ -105,14 +94,10 @@ class MortgageCalculator: UIViewController {
         
         let monthlyPaymentFigureThousands = monthlyPaymentFigure.thousands()
         
-        print(monthlyPaymentFigureThousands)
-        
         monthlyPayment.text = "£" + monthlyPaymentFigureThousands
           
     }
     
-    
-
     
     func calculateStampDuty() {
         
@@ -124,20 +109,22 @@ class MortgageCalculator: UIViewController {
             let bandFourTopFTB = propertyValue - 1500000
             let bandFourPercentageFTB = (bandFourTopFTB * 12) / 100
             let bandFourTotalFTB = 33750 + 57500 + bandFourPercentageFTB
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandFourTotalFTB))
+//            let bandFourTotalFTBThousands = bandFourPercentageFTB.thousands()
+//            stampDuty.text = "£" + String(format: "%.0f", Double(bandFourTotalFTB))
+            stampDuty.text = "£" + bandFourTotalFTB.thousands()
         } else if borrowingDropdown.currentTitle == "First Time Buyer" && propertyValue > 925000 && propertyValue <= 1500000 {
             let bandThreeTopFTB = propertyValue - 925000
             let bandThreePercentageFTB = (bandThreeTopFTB * 10) / 100
             let bandThreeTotalFTB = 33750 + bandThreePercentageFTB
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandThreeTotalFTB))
+            stampDuty.text = "£" + bandThreeTotalFTB.thousands()
         } else if borrowingDropdown.currentTitle == "First Time Buyer" && propertyValue > 625000 && propertyValue <= 925000 {
             let bandTwoTopFTB = propertyValue - 625000
             let bandTwoPercentageFTB = ((bandTwoTopFTB * 5) / 100) + 18750
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandTwoPercentageFTB))
+            stampDuty.text = "£" + bandTwoPercentageFTB.thousands()
         } else if borrowingDropdown.currentTitle == "First Time Buyer" && propertyValue > 425000 && propertyValue <= 625000 {
             let bandTwoTopFTB = propertyValue - 425000
             let bandTwoPercentageFTB = (bandTwoTopFTB * 5) / 100
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandTwoPercentageFTB))
+            stampDuty.text = "£" + bandTwoPercentageFTB.thousands()
         }
         
         //MARK: - ADDITIONAL PROPERTY (ADDP)
@@ -147,20 +134,20 @@ class MortgageCalculator: UIViewController {
             let bandFourTopADDP = propertyValue - 1500000
             let bandFourPercentageADDP = (bandFourTopADDP * 15) / 100
             let bandFourTotalADDP = 7500 + 54000 + 74750 + bandFourPercentageADDP
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandFourTotalADDP))
+            stampDuty.text = "£" + bandFourTotalADDP.thousands()
         } else if borrowingDropdown.currentTitle == "Additional Property" && propertyValue > 925000 && propertyValue <= 1500000 {
             let bandThreeTopADDP = propertyValue - 925000
             let bandThreePercentageADDP = (bandThreeTopADDP * 13) / 100
             let bandThreeTotalADDP = 7500 + 54000 + bandThreePercentageADDP
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandThreeTotalADDP))
+            stampDuty.text = "£" + bandThreeTotalADDP.thousands()
         } else if borrowingDropdown.currentTitle == "Additional Property" && propertyValue > 250000 && propertyValue <= 925000 {
             let bandTwoTopADDP = propertyValue - 250000
             let bandTwoPercentageADDP = (bandTwoTopADDP * 8) / 100
             let bandTwoTotalADDP = 7500 + bandTwoPercentageADDP
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandTwoTotalADDP))
+            stampDuty.text = "£" + bandTwoTotalADDP.thousands()
         } else if borrowingDropdown.currentTitle == "Additional Property" && propertyValue <= 250000 {
             let bandOnePercentageADDP = (propertyValue * 3) / 100
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandOnePercentageADDP))
+            stampDuty.text = "£" + bandOnePercentageADDP.thousands()
         }
 
         //MARK: - BUYING NEXT HOME
@@ -170,16 +157,16 @@ class MortgageCalculator: UIViewController {
             let bandFourTop = propertyValue - 1500000
             let bandFourPercentage = (bandFourTop * 12) / 100
             let bandFourTotal = 33750 + 57500 + bandFourPercentage
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandFourTotal))
+          stampDuty.text = "£" + bandFourTotal.thousands()
         } else if borrowingDropdown.currentTitle == "Moving Home" && propertyValue > 925000 && propertyValue <= 1500000 {
             let bandThreeTop = propertyValue - 925000
             let bandThreePercentage = (bandThreeTop * 10) / 100
             let bandThreeTotal = 33750 + bandThreePercentage
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandThreeTotal))
+            stampDuty.text = "£" + bandThreeTotal.thousands()
         } else if borrowingDropdown.currentTitle == "Moving Home" && propertyValue > 250000 && propertyValue <= 925000 {
             let bandTwoTop = propertyValue - 250000
             let bandTwoPercentage = (bandTwoTop * 5) / 100
-            stampDuty.text = "£" + String(format: "%.0f", Double(bandTwoPercentage))
+            stampDuty.text = "£" + bandTwoPercentage.thousands()
         } else {
             stampDuty.text = "£" + String(format: "%.0f", 0)
         }
@@ -196,6 +183,8 @@ class MortgageCalculator: UIViewController {
         loanToValue.text = String(format: "%.1f", Double(ltv)) + "%"
         
     }
+    
+    //MARK: - ACTIONS
         
     @IBAction func dropdownClicked(_ sender: Any) {
         dataSource = ["First Time Buyer", "Moving Home", "Remortgage       ", "Additional Property"]
@@ -205,17 +194,12 @@ class MortgageCalculator: UIViewController {
     
     
     @IBAction func calculatePressed(_ sender: Any) {
-        
         calculateMonthlyRepayment()
         calculateStampDuty()
         calculateLTV()
-        
     }
-    
-    
 
     @IBAction func resetPressed(_ sender: Any) {
-        
         borrowingDropdown.titleLabel?.text = "Please Select"
         interestTextField.text = ""
         loanTextField.text = ""
@@ -223,10 +207,8 @@ class MortgageCalculator: UIViewController {
         valueTextField.text = ""
         monthlyPayment.text = "£0.00"
         stampDuty.text = "£0"
-        loanToValue.text = "0%"
-        
+        loanToValue.text = "0"
     }
-    
     
     @IBAction func disclaimerPressed(_ sender: Any) {
         performSegue(withIdentifier: "goToDisclaimer", sender: self)
@@ -240,15 +222,13 @@ class MortgageCalculator: UIViewController {
     
 }
 
+//MARK: - EXTENSIONS
+
 extension MortgageCalculator: UITextFieldDelegate {
-
-
-    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
         view.endEditing(true)
     }
-
 }
 
 extension MortgageCalculator: UITableViewDelegate, UITableViewDataSource {
