54bcc447e4e051a125ceafb7ca98287e6f90fd61
James Penny <james_penny@hotmail.co.uk>
2023-03-24 16:15:19 +0000

    Applied constraints to MortgageCalculator VC.

--- Mortgage Calculator/Controller/MortgageCalculator.swift
+++ Mortgage Calculator/Controller/MortgageCalculator.swift
@@ -110,14 +110,14 @@ class MortgageCalculator: UIViewController {
         self.view.addSubview(tableView)
         tableView.layer.cornerRadius = 5
             
-            transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+        transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
         tableView.reloadData()
             let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
             transparentView.addGestureRecognizer(tapgesture)
             transparentView.alpha = 0
             UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
                 self.transparentView.alpha = 0.5
-                self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: CGFloat(self.dataSource.count * 50))
+                self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: CGFloat(self.dataSource.count * 60))
             }, completion: nil)
         }
         
